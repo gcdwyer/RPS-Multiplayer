87fd8038300a02f6ef4afb6e36ba9100bff36270
gcdwyer <gcdwyer@gmail.com>
2017-08-27 21:46:51 -0400

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,15 +15,12 @@ var database = firebase.database();
 
 var player1 = null;
 var player2 = null;
-
 var player1Name = "";
 var player2Name = "";
-
-var playerName = "";
-
+var pickedName = "";
 var turn = 1;
 
-// CLICK EVENT ===========================================================================
+// Event Handler - Add Player Button =================================================================
 
 $("#addButton").on("click", function(event) {
 
@@ -36,9 +33,9 @@ $("#addButton").on("click", function(event) {
 	// if player 1 doesn't exist, create it
 	} else if (player1 === null) {
 		console.log("About to add P1");
-		playerName = $("#nameInput").val().trim();
+		pickedName = $("#nameInput").val().trim();
 		player1 = {
-			name: playerName,
+			name: pickedName,
 			win: 0,
 			loss: 0,
 			tie: 0
@@ -50,9 +47,9 @@ $("#addButton").on("click", function(event) {
 	} else if (player1 !== null && player2 === null) {
 
 		console.log("About to add P2");
-		playerName = $("#nameInput").val().trim();
+		pickedName = $("#nameInput").val().trim();
 		player2 = {
-			name: playerName,
+			name: pickedName,
 			win: 0,
 			loss: 0,
 			tie: 0
@@ -64,55 +61,32 @@ $("#addButton").on("click", function(event) {
 	// clears the name field
 	$("#nameInput").val("");
 
-	// Chat Join Game ========================================================
-	console.log("playerName: " + playerName);
-
-	// add player to message when joining
-	var joined = playerName + " has joined the chat!";
-
+	// add player to chat when joining
+	var joined = pickedName + " has joined the chat!";
 	database.ref("/chat/").push(joined);
-
-	// $("#chatDisplay").append(joined + "<br>");
-
 })
 
-// Chat =========================================================================
+// Event Handler - Chat Send Button =================================================================
 
 $("#chatSend").on("click", function(event) {
-
 	event.preventDefault();
-
 	var msg = $("#textField").val().trim();
-
-	var message = playerName + ": " + msg;
-
-	console.log("message: " + message);
-
-	console.log("playerName: " + playerName);
-
+	var message = pickedName + ": " + msg;
 	database.ref("/chat/").push(message);
-	// $("#chatDisplay").append(playerName + ": " + message + "<br>");
-
 	$("#textField").val("");
-
 })
 
-// Add listener to DB to check for any changes ========================================================
+// Event Listener for Players in DB ====== ========================================================
 
 database.ref("/players/").on("value", function (snapshot) {
 
 	console.log(snapshot.val());
 
 	if (snapshot.child("player1").exists()) {
-
 		console.log("P1 exists");
-
 		player1 = snapshot.val().player1;
-
 		player1Name = player1.name;
-
 		$("#playerPanel1").html(player1Name);
-
 		$("#player1Stats").html("Win: " + player1.win + " | Loss: " + player1.loss + " | Tie: " + player1.tie);
 
 	} else {
@@ -121,17 +95,11 @@ database.ref("/players/").on("value", function (snapshot) {
 		console.log("P1 does NOT exist");
 	}
 
-
 	if (snapshot.child("player2").exists()) {
-
 		console.log("P2 exists");
-
 		player2 = snapshot.val().player2;
-
 		player2Name = player2.name;
-
 		$("#playerPanel2").html(player2.name);
-
 		$("#player2Stats").html("Win: " + player2.win + " | Loss: " + player2.loss + " | Tie: " + player2.tie);
 
 	} else {
@@ -140,115 +108,75 @@ database.ref("/players/").on("value", function (snapshot) {
 		console.log("P2 does NOT exist");
 	}
 
-
 	if (player1 !== null && player2 !== null) {
-
-		// $("#Status").text("Waiting on " + player1Name + " to choose...");
-
 		$("#Status").text("Waiting on " + player1.name + " to make a selection");
-
 	}
-
 });
 
-// Listener on CHAT for any changes
+// Event listener for Chat in DB ====== ========================================================
+
 database.ref("/chat/").on("child_added", function(snapshot) {
-
 	var messages = snapshot.val();
-
 	console.log(messages);
-
 	$("#chatDisplay").append(messages + "<br>");
-
 });
 
 // Mouse over to change display =============================================================
 
 	$(".panel1Option").on("mouseover", function () {
-
 	 		$(this).css("background-color", "lightgray");
-
 	 	})
 
 	$(".panel1Option").on("mouseout", function () {
-
 	 		$(this).css("background-color", "white");
-
 	 	})
 
 	$(".panel2Option").on("mouseover", function () {
-
 	 		$(this).css("background-color", "lightgray");
-
 	 	})
 
 	$(".panel2Option").on("mouseout", function () {
-
 	 		$(this).css("background-color", "white");
-
 	 	})
 
 
-// Player 1 choice =======================================================================
+// Event Handler - Player 1 pick =============================================================
 
 $("#player1Panel").on("click", ".panel1Option", function() {
 
 	// event.preventDefault();
 
 	if (player1 !== null && player2 !== null && turn === 1) {
-
-		console.log("RPS was chosen");
-
 		var choice = $(this).text().trim();
-
 		console.log("p1 choice: " + choice);
-
 		database.ref("/players/player1/choice").set(choice);
-
 		$(".panel1Option").hide();
-
 		$("#playerPanel1").append("<br><br><br><h1>" + choice + "</h1><br><br>");
-
 		turn = 2;
-
 		database.ref().child("/players/turn").set(turn);
-
 		console.log("Player " + turn + " turn");
-
 		$("#Status").text("Waiting on " + player2.name + " to make a selection");
-
 	}
-
 })
 
-// Player 2 choice =======================================================================
+// Event Handler - Player 2 pick ============================================================
 
 $("#player1Pane2").on("click", ".panel2Option", function() {
 
 	// event.preventDefault();
 
 	if (player1 !== null && player2 !== null && turn === 2) {
-
-		console.log("RPS was chosen");
-
 		var choice = $(this).text().trim();
-
 		console.log("p2 choice: " + choice);
-
 		database.ref("/players/player2/choice").set(choice);
-
 		$(".panel2Option").hide();
-
 		$("#playerPanel2").html("<br><br><br><h1>" + choice + "</h1><br><br>");
 
 		compare();
-
 	}
-
 })
 
-
-// RPS Compare =========================================================================
+// RPS Compare ==============================================================================
 
 function compare() {
 
@@ -260,126 +188,88 @@ function compare() {
 	var p1choice = player1.choice;
 	var p2choice = player2.choice;
 
-	// $(".panel1Option").show();
-	// $(".panel2Option").show();
-
 	$("#Status").show();
 
 	if (p1choice === "Rock") {
 
 		if (p2choice === "Rock") {
-
 			console.log("Tie Game");
 			database.ref("/players/player1/tie").set(player1.tie + 1);
 			database.ref("/players/player2/tie").set(player2.tie + 1);
 			$("#Status").html("<br><br><br><h2>Tie</h2>");
 
 		} else if (p2choice === "Paper") {
-
 			console.log("Paper Wins");
 			database.ref("/players/player1/loss").set(player1.loss + 1);
 			database.ref("/players/player2/win").set(player2.win + 1);
 			$("#Status").html("<br><br><br><h2>" + player2Name + " wins!</h2>");
 
 		} else { //Scissors
-
 			console.log("Rock Wins");
 			database.ref("/players/player1/win").set(player1.win + 1);
 			database.ref("/players/player2/loss").set(player2.loss + 1);
 			$("#Status").html("<br><br><br><h2>" + player1Name + " wins!</h2>");
-
 		}
-
 	} 
 
 	else if (p1choice === "Paper") {
 
 		if (p2choice === "Rock") {
-
 			console.log("Paper Wins");
 			database.ref("/players/player1/win").set(player1.win + 1);
 			database.ref("/players/player2/loss").set(player2.loss + 1);
 			$("#Status").html("<br><br><br><h2>" + player1Name + " wins!</h2>");
 
 		} else if (p2choice === "Paper") {
-
 			console.log("Tie Game");
 			database.ref("/players/player1/tie").set(player1.tie + 1);
 			database.ref("/players/player2/tie").set(player2.tie + 1);
 			$("#Status").html("<br><br><br><h2>Tie</h2>");
 
 		} else { //Scissors
-
 			console.log("Scissors Win");
 			database.ref("/players/player1/loss").set(player1.loss + 1);
 			database.ref("/players/player2/win").set(player2.win + 1);
 			$("#Status").html("<br><br><br><h2>" + player2Name + " wins!</h2>");
-
 		}
-
 	} 
 
 	else if (p1choice === "Scissors") {
 
 		if (p2choice === "Rock") {
-
 			console.log("Rock Wins");
 			database.ref("/players/player1/loss").set(player1.loss + 1);
 			database.ref("/players/player2/win").set(player2.win + 1);
 			$("#Status").html("<br><br><br><h2>" + player2Name + " wins!</h2>");
 
 		} else if (p2choice === "Paper") {
-
 			console.log("Scissors Win");
 			database.ref("/players/player1/win").set(player1.win + 1);
 			database.ref("/players/player2/loss").set(player2.loss + 1);
 			$("#Status").html("<br><br><br><h2>" + player1Name + " wins!</h2>");
 
 		} else { //Scissors
-
 			console.log("Tie Game");
 			database.ref("/players/player1/tie").set(player1.tie + 1);
 			database.ref("/players/player2/tie").set(player2.tie + 1);
 			$("#Status").html("<br><br><br><h2>Tie</h2>");
-
 		}
-
 	}
 
-	// RESET Timeout
+	// RESET Timeout =====================================================================
 
 	setTimeout(reset, 1000 * 3);
 
 	function reset() {
 
-		console.log("Reset after 3 seconds");
-
-		// $("#playerPanel1 text").last().remove();
-
+		console.log("Game reset after 3 seconds");
 		$("#playerPanel2 text").last().remove();
-
 		$(".panel1Option").show();
-
 		$(".panel2Option").show();
-
 		$("#Status").hide();
-
-		console.log("I got here");
-
-
-
-
 	}
-
-
 
 	turn = 1;
 
-	// database.ref().child("/players/turn").set(turn);
-
 	console.log("Player " + turn + " turn");
-
 }
-
-
-// Reset game after ~3 seconds
